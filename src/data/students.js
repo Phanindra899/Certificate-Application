a10c0c855d73e81a704ca59e318534ae25ede56f
Phanindra899 <p14410766@gmail.com>
2026-07-19 22:08:39 +0530

    Added Data Analytics student records

--- src/data/students.js
+++ src/data/students.js
@@ -79,6 +79,41 @@ export const students = [
   remarks:
     "Demonstrated strong dedication, technical skills, and a positive learning attitude throughout the internship. Successfully completed all assigned tasks with consistency and professionalism."
 },
+{
+  verificationId: "CITS6134",
+  studentName: "DUDEKULA KHAJA HUSSAIN",
+  courseName: "Data Analytics",
+  category: "Internship",
+  duration: "8 Weeks",
+  companyName: "CODTECH IT SOLUTIONS PRIVATE LIMITED",
+  startDate: "06 May 2026",
+  endDate: "01 July 2026",
+  verificationDate: "06 May 2026"
+},
+
+{
+  verificationId: "CITS4278",
+  studentName: "GOLLA BHARATH",
+  courseName: "Data Analytics",
+  category: "Internship",
+  duration: "8 Weeks",
+  companyName: "CODTECH IT SOLUTIONS PRIVATE LIMITED",
+  startDate: "06 May 2026",
+  endDate: "01 July 2026",
+  verificationDate: "06 May 2026"
+},
+
+{
+  verificationId: "CITS8951",
+  studentName: "Jakkala Raju",
+  courseName: "Data Analytics",
+  category: "Internship",
+  duration: "8 Weeks",
+  companyName: "CODTECH IT SOLUTIONS PRIVATE LIMITED",
+  startDate: "06 May 2026",
+  endDate: "01 July 2026",
+  verificationDate: "06 May 2026"
+},
 ];
 
 export function findStudentById(verificationId) {
